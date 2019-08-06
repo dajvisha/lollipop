bc73261ebd07cf916021f5d8c92eec89d8f52718
diegoajv <diegojosevilla@outlook.com>
2019-08-06 00:51:13 -0500

    Feat: Add behaviour to select gradient type

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,12 +1,39 @@
 import React from 'react';
+import Button from '../widgets/Button';
+import constants from '../../helpers';
 import './Menu.css';
 
-function Menu() {
-  return (
-    <div className="menu">
-      <div className="logo"><span role="img">🍭</span> Lollipod</div>
-    </div>
-  );
+class Menu extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  changeGradientType = (type) => {
+    this.props.toggleChangeGradient({gradientType: type});
+  }
+
+  render() {
+    return (
+      <div className="menu">
+        <div className="logo"><span role="img">🍭</span> Lollipod</div>
+        <div className="menuSection">
+          <p>Style</p>
+          <Button label="Linear"
+                  selected={this.props.gradientType == constants.GRADIENT_TYPE.LINEAR}
+                  onClick={(_) => this.changeGradientType(constants.GRADIENT_TYPE.LINEAR)} />
+          <Button label="Radial"
+                  selected={this.props.gradientType == constants.GRADIENT_TYPE.RADIAL} 
+                  onClick={(_) => this.changeGradientType(constants.GRADIENT_TYPE.RADIAL)} />
+        </div>
+        <div className="menuSection">
+          <p>Direction</p>
+        </div>
+        <div className="menuSection">
+          <p>Colors</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Menu;
