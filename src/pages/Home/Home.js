8b3990dad44f677927c6338876e2a8cb2abb7fb5
diegoajv <diegojosevilla@outlook.com>
2019-08-05 20:52:07 -0500

    Chore: Configure pages directory

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="App">
+      Welcome to Lollipop.
+    </div>
+  );
+}
+
+export default Home;
