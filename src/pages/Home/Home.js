ddb4cc77f3f883d875da3aee72a8e99679b0d799
diegoajv <diegojosevilla@outlook.com>
2019-08-05 23:08:55 -0500

    Feat: Add basic structure for gradient background generator

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,12 +2,32 @@ import React from 'react';
 import Menu from './../../components/Menu';
 import './Home.css';
 
-function Home() {
-  return (
-    <div>
-      <Menu />
-    </div>
-  );
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gradientType: "",
+      gradientDirection: "",
+      gradientColorOne: "",
+      gradientColorTwo: "",
+      gradient: {
+        backgroundImage: "linear-gradient(#F8D353, #FF5C5A)"
+      }
+    }
+  }
+
+  toggleChangeGradient = (attribute) => {
+    this.setState(state => (attribute));
+  }
+
+  render() {
+    return (
+      <div style={this.state.gradient}>
+        <Menu toggleChangeGradient={this.toggleChangeGradient}/>
+      </div>
+    );
+  }
 }
 
 export default Home;
