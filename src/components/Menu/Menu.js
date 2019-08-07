3449a6e3909397380423a5d451c21e2c61747a6c
diegoajv <diegojosevilla@outlook.com>
2019-08-06 20:16:06 -0500

    Feat: Add gradient generator options

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import Button from '../widgets/Button';
-import constants from '../../helpers';
 import './Menu.css';
+import StyleOptions from './Options/StyleOptions';
+import DirectionOptions from './Options/DirectionOptions';
+import ColorOptions from './Options/ColorOptions';
 
 class Menu extends React.Component {
   constructor(props) {
@@ -13,32 +14,22 @@ class Menu extends React.Component {
       <div className="menu">
         <div className="logo"><span role="img">🍭</span> Lollipod</div>
         <div className="menuSection">
+          <p>Colors</p>
+          <ColorOptions colorOne={this.props.gradientColorOne}
+                        colorTwo={this.props.gradientColorTwo}
+                        toggleChangeColor={this.props.toggleChangeColor}/>
+        </div>
+        <div className="menuSection">
           <p>Style</p>
-          {styleOptions(this.props)}
+          {StyleOptions(this.props)}
         </div>
         <div className="menuSection">
           <p>Direction</p>
-        </div>
-        <div className="menuSection">
-          <p>Colors</p>
+          {DirectionOptions(this.props)}
         </div>
       </div>
     );
   }
-}
-
-function styleOptions(props) {
-  let { gradientType, toggleChangeGradient } = props; 
-  return (
-    <div>
-      <Button label="Linear"
-              selected={gradientType === constants.GRADIENT_TYPE.LINEAR}
-              onClick={(_) => toggleChangeGradient({gradientType: constants.GRADIENT_TYPE.LINEAR})} />
-      <Button label="Radial"
-              selected={gradientType === constants.GRADIENT_TYPE.RADIAL} 
-              onClick={(_) => toggleChangeGradient({gradientType: constants.GRADIENT_TYPE.RADIAL})} />
-    </div>
-  );
 }
 
 export default Menu;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -10,11 +10,20 @@ class Home extends React.Component {
     this.state = {
       gradientType: constants.GRADIENT_TYPE.LINEAR,
       gradientDirection: constants.GRADIENT_DIRECTION.LINEAR.TOP,
-      gradientColorOne: "",
-      gradientColorTwo: "",
+      gradientColorOne: "#FF5C5A",
+      gradientColorTwo: "#F8D353",
       gradient: {
         backgroundImage: "linear-gradient(#F8D353, #FF5C5A)"
       }
+    }
+  }
+
+  toggleChangeColor = (color, index) => {
+    if (index === 1) {
+      this.setState({gradientColorOne: color}, () => {this.createGradient()});
+    } 
+    if (index === 2) {
+      this.setState({gradientColorTwo: color}, () => {this.createGradient()});
     }
   }
 
@@ -23,7 +32,7 @@ class Home extends React.Component {
   }
 
   createGradient = () => {
-    let gradient = `${this.state.gradientType}(${this.state.gradientDirection}, #F8D353, #FF5C5A)`;
+    let gradient = `${this.state.gradientType}(${this.state.gradientDirection}, ${this.state.gradientColorOne}, ${this.state.gradientColorTwo})`;
     this.setState({gradient: {backgroundImage: gradient}});
   }
 
@@ -32,6 +41,10 @@ class Home extends React.Component {
       <div className="container">
         <Menu className="menu"
               gradientType={this.state.gradientType}
+              gradientDirection={this.state.gradientDirection}
+              gradientColorOne={this.state.gradientColorOne}
+              gradientColorTwo={this.state.gradientColorTwo}
+              toggleChangeColor={this.toggleChangeColor}
               toggleChangeGradient={this.toggleChangeGradient}/>
         <div className="gradient" style={this.state.gradient}></div>
       </div>
