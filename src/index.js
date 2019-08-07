4b19a21f61f0fd12ab7605156dda016b744ea77e
diegoajv <diegojosevilla@outlook.com>
2019-08-06 20:34:15 -0500

    Refactor: Fix project structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Home from './pages/Home';
+import Home from './pages/Home/Home';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Home />, document.getElementById('root'));
